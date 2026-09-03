5cd9c0fab69f9e7d45aea7a0d56b225c7e7f3515
chiran <chiranjibipunmagar369@gmail.com>
2026-09-03 20:48:43 +0545

    import and export in js

---
+++ day 20/script.js
@@ -0,0 +1,7 @@
+console.log("import and export")
+import obj from './script2.js'
+import {b,c,d} from './script2.js'
+const a=5
+console.log(a)
+console.log(obj)
+console.log(b,c,d)
---
+++ day 20/script2.js
@@ -0,0 +1,9 @@
+export const b=6
+ 
+const obj={
+    name:"ram",
+    age:16
+}
+
+export default obj
+// export default b
